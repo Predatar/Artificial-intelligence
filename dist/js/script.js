3771f2ef8afc8e7f529aa954ad37fd6111798793
Maksym Martynenko <predatar24@gmail.com>
2022-10-10 17:30:42 +0300

    initial commit

---
+++ dist/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+   $('.slick-carousel').slick({
+      slidesToShow: 3,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      arrows: false,
+      dots: true
+   });
+});
